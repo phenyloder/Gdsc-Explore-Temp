a4c0be8211343522ce5b25b505156ba9fb248924
Vinayak Das <vinayakd909@gmail.com>
2021-12-31 02:04:45 +0530

    Add Header Section

---
+++ components/header/Header.js
@@ -0,0 +1,38 @@
+export default function Header(){
+    return (
+        <>
+            <br />
+            <br />
+            <br />
+            <div className="flex justify-center">
+                <div className="h-auto w-[862px] text-center font-sans text-[34px] font-light text-[#373F41] leading-[3rem]">
+                Lorem ipsum dolor sit <span className="text-[#4285F4]">amet</span>, consectetur adipiscing elit. <br /> Enim nec <span className="text-[#EA4335]">tempus faucibus</span> volutpat enim a. <span className="text-[#FBBC05]">Tempor</span>, a <span className="text-[#34A853]">elementum</span> et, urna turpis congue
+                </div>
+            </div>
+            <br />
+            <br />
+            <br />
+            <div className="relative bg-[#9B9B9B]/[.06] h-[655px]">
+                <div className="flex flex-col" >
+                    <div className="relative left-[10.3rem] top-[9.75rem]">
+                        <h2 className="font-sans font-normal h-[144px] w-[740px] text-[60px] text-[#373F41] leading-[4.5rem]">
+                            Lorem ipsum dolor sit amet consectetur adipiscing
+                        </h2>
+                    </div>
+                    <div className="relative left-[10.3rem] top-48">
+                        <p className="font-sans font-normal h-[84px] w-[647px] text-xl text-[#737B7D] ">
+                            Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te. Ex duo eripuit mentitum.
+                        </p>
+                    </div>
+                    <button className="relative left-[10.3rem] top-60 w-[8.6rem] h-[3rem] bg-[#3C64B1] rounded font-sans text-lg text-white cursor-pointer ">Lorem</button>
+                </div>
+                <div className="absolute left-[60.25rem] top-[65px] h-[524px] w-[845px] border-[1.34722px] border-solid border-[#3C64B1] rounded-md ">
+                    
+                </div>
+            </div>
+            <br />
+            <br />
+            <br />
+        </>
+    )
+}
