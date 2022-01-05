2ccb6d4b3e3d9882c60af008851b178b3c47b26a
Vinayak Das <vinayakd909@gmail.com>
2022-01-05 14:34:06 +0530

    Add Responsiveness

--- components/header/Header.js
+++ components/header/Header.js
@@ -5,28 +5,29 @@ export default function Header(){
             <br />
             <br />
             <div className="flex justify-center">
-                <div className="h-auto w-[53.875rem] text-center font-sans text-[2.125rem] font-light text-[#373F41] leading-[3rem]">
+                <div className="h-auto w-[53.875rem] text-center font-sans text-[1.75rem] font-light p-10 text-[#373F41] md:h-auto md:w-[53.875rem] md:text-center md:font-sans md:text-[2.125rem] md:font-light md:text-[#373F41] md:leading-[3rem]">
                 Lorem ipsum dolor sit <span className="text-[#4285F4]">amet</span>, consectetur adipiscing elit. <br /> Enim nec <span className="text-[#EA4335]">tempus faucibus</span> volutpat enim a. <span className="text-[#FBBC05]">Tempor</span>, a <span className="text-[#34A853]">elementum</span> et, urna turpis congue
                 </div>
             </div>
             <br />
             <br />
             <br />
-            <div className="relative flex bg-[#9B9B9B]/[.06] h-[40.9375rem]">
-                <div className="flex flex-col items-center text-center p-4 md:flex md:flex-col md:w-1/2 md:items-start md:text-left" >
-                    <div className="relative top-[4rem] md:relative md:left-[10.3rem] md:top-[9.75rem]">
-                        <h2 className="font-sans font-normal text-[2.75rem] text-[#373F41] md:font-sans md:font-normal md:h-[9rem] md:w-[46.25rem] md:text-[3.75rem] md:text-[#373F41] md:leading-[4.5rem]">
-                            Lorem ipsum dolor sit amet consectetur adipiscing
+            <div className="relative flex bg-[#9B9B9B]/[.06] h-[42rem] md:relative md:flex md:bg-[#9B9B9B]/[.06] md:h-[40.9375rem]">
+                <div className="flex flex-col items-center p-8 md:flex md:flex-col md:w-1/2 md:items-start md:text-left" >
+                    <div className="relative top-[2.5rem] md:relative md:left-[10.3rem] md:top-[5rem]">
+                        <h2 className="font-sans font-medium text-[2.75rem] text-[#373F41] md:font-sans md:font-normal md:h-[9rem] md:w-[46.25rem] md:text-[3.75rem] md:text-[#373F41] md:leading-[4.5rem]">
+                        DSC Explore
+                        <h3 className="font-sans font-normal text-[1.5rem] text-[#373F41] md:font-sans md:font-normal md:h-[9rem] md:w-[46.25rem] md:text-[2rem] md:text-[#373F41]">Explore the Developers from the North-East</h3>
                         </h2>
                     </div>
-                    <div className="relative top-[6rem] md:relative md:left-[10.3rem] md:top-48">
-                        <p className="font-sans font-normal h-[5.25rem] text-xl text-[#737B7D] md:font-sans md:font-normal md:h-[5.25rem] md:w-[40.4375rem] md:text-xl md:text-[#737B7D]">
-                            Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te. Ex duo eripuit mentitum.
+                    <div className="relative top-[4rem] md:relative md:left-[10.3rem] md:top-24">
+                        <p className="font-sans font-normal h-[5.25rem] text-base text-[#737B7D] md:font-sans md:font-normal md:h-[5.25rem] md:w-[40.4375rem] md:text-xl md:text-[#737B7D]">
+                        Google Developer Clubs all across the northeast(Assam Engineering College, IIIT Guwahati,NIT Agartala, NIT Silchar) is enthusiasticatic to announce  the highly anticipated second edition of DSC Explore consisting of a large plethora of events for developers.<br/><br/>It will provide a platform for developers to engage and learn by participating in various  activities and workshops earning badges and certificates along the way.
                         </p>
                     </div>
-                    <button className="relative top-[12rem] w-[8.6rem] h-[3rem] bg-[#3C64B1] rounded font-sans text-lg text-white cursor-pointer md:relative md:left-[10.3rem] md:top-60 md:w-[8.6rem] md:h-[3rem] md:bg-[#3C64B1] md:rounded md:font-sans md:text-lg md:text-white md:cursor-pointer ">Lorem</button>
+                    <button className="relative top-[21rem] w-[8.6rem] h-[3rem] bg-[#3C64B1] rounded font-sans text-lg text-white cursor-pointer  hover:shadow-xl md:relative md:left-[10.3rem] md:top-[17rem] md:w-[8.6rem] md:h-[3rem] md:bg-[#3C64B1] md:rounded md:font-sans md:text-lg md:text-white md:cursor-pointer ">Register Now</button>
                 </div>
-                <div className="hidden xl:flex xl:w-1/2 xl:justify-center">
+                <div className="hidden 2xl:flex 2xl:w-1/2 2xl:justify-center">
                     <div className="relative top-[4.0625rem] h-[32.75rem] w-[52.8125rem] border-[1.34722px] border-solid border-[#3C64B1] rounded-md ">
 
                     </div>
